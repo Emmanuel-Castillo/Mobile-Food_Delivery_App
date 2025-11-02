6d9ad73c4c50a934560679e7d5696df6c3855c2e
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-11-02 10:04:28 -0600

    Designed auth screens

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.manny.food_delivery_app",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: "69077276000e126afbd0",
+    userCollectionId: "user"
+}
